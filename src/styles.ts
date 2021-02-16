d480ac85dc467efdc1c7cbac30fcdd441320d8f3
nikita.golovchenko <nikita.golovchenko@p2h.com>
2021-02-16 20:13:41 +0200

    create auth actions

---
+++ src/styles.ts
@@ -0,0 +1,12 @@
+import { makeStyles, Theme } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  wrapper: {
+    position: 'relative',
+    width: '100%',
+    overflow: 'hidden',
+    fontSize: '1rem',
+  },
+}));
+
+export default useStyles;
