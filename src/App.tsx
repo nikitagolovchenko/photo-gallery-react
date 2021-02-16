16c8feddccc508a80852300c3181e57ca46c6e60
nikita.golovchenko <nikita.golovchenko@p2h.com>
2021-02-16 14:54:33 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline } from '@material-ui/core';
+import React, { FC } from 'react';
+import useStyles from './styles';
 
-function App() {
+const App: FC = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.wrapper}>
+      <CssBaseline />
+      <div>Hello World!</div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  wrapper: {
+    position: 'relative',
+    width: '100%',
+    overflow: 'hidden',
+    fontSize: '1rem'
+  },
+});
+
+export default useStyles;
